ac338c0eefd94616e00084b7bb400c5b2a87a4c2
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-13 15:22:12 +0200

    learn nested types

--- starter.playground/Pages/Nested types.xcplaygroundpage/Contents.swift
+++ starter.playground/Pages/Nested types.xcplaygroundpage/Contents.swift
@@ -27,16 +27,24 @@
 /// THE SOFTWARE.
 import Foundation
 
-struct Toy {
-  var name: String
+struct Toy: Codable {
+    var name: String
 }
 
-struct Employee {
-  var name: String
-  var id: Int
-  var favoriteToy: Toy
+struct Employee: Codable {
+    var name: String
+    var id: Int
+    var favoriteToy: Toy
 }
 
 let toy = Toy(name: "Teddy Bear")
 let employee = Employee(name: "John Appleseed", id: 7, favoriteToy: toy)
 
+let encoder = JSONEncoder()
+encoder.outputFormatting = .prettyPrinted
+let decoder = JSONDecoder()
+
+let data = try encoder.encode(employee)
+let string = String(data: data, encoding: .utf8)!
+
+let sameEmployee = try decoder.decode(Employee.self, from: data)
