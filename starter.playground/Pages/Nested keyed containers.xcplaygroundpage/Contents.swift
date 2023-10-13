7273b2ba498bfbb49d478fd0d44ccb44bf8bc8e5
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-13 15:46:02 +0200

    learn nested keyed containers

--- starter.playground/Pages/Nested keyed containers.xcplaygroundpage/Contents.swift
+++ starter.playground/Pages/Nested keyed containers.xcplaygroundpage/Contents.swift
@@ -28,13 +28,41 @@
 import Foundation
 
 struct Toy: Codable {
-  var name: String
+    var name: String
 }
 
 struct Employee: Encodable {
-  var name: String
-  var id: Int
-  var favoriteToy: Toy
+    var name: String
+    var id: Int
+    var favoriteToy: Toy
+
+    enum CodingKeys: CodingKey {
+        case name, id, gift
+    }
+
+    enum GiftKeys: CodingKey {
+        case toy
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(id, forKey: .id)
+        var giftContainer = container
+            .nestedContainer(keyedBy: GiftKeys.self, forKey: .gift)
+        try giftContainer.encode(favoriteToy, forKey: .toy)
+    }
+}
+
+extension Employee: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        id = try container.decode(Int.self, forKey: .id)
+        let giftContainer = try container
+            .nestedContainer(keyedBy: GiftKeys.self, forKey: .gift)
+        favoriteToy = try giftContainer.decode(Toy.self, forKey: .toy)
+    }
 }
 
 let toy = Toy(name: "Teddy Bear")
@@ -46,3 +74,4 @@ let decoder = JSONDecoder()
 let nestedData = try encoder.encode(employee)
 let nestedString = String(data: nestedData, encoding: .utf8)!
 
+let sameEmployee = try decoder.decode(Employee.self, from: nestedData)
