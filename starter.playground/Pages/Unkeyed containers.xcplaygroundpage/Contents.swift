7ca98757197b9219f57712d3eb56df37cc1f5f2d
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-13 16:01:48 +0200

    learn unkeyed containers encoding and decoding

--- starter.playground/Pages/Unkeyed containers.xcplaygroundpage/Contents.swift
+++ starter.playground/Pages/Unkeyed containers.xcplaygroundpage/Contents.swift
@@ -28,7 +28,7 @@
 import Foundation
 
 struct Toy: Codable {
-  var name: String
+    var name: String
 }
 
 let toy = Toy(name: "Teddy Bear")
@@ -37,9 +37,29 @@ let encoder = JSONEncoder()
 let decoder = JSONDecoder()
 
 struct Label: Encodable {
-  var toy: Toy
+    var toy: Toy
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+        try container.encode(toy.name.lowercased())
+        try container.encode(toy.name.uppercased())
+        try container.encode(toy.name)
+    }
 }
 
 let label = Label(toy: toy)
 let labelData = try encoder.encode(label)
 let labelString = String(data: labelData, encoding: .utf8)!
+
+extension Label: Decodable {
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        var name = ""
+        while !container.isAtEnd {
+            name = try container.decode(String.self)
+        }
+        toy = Toy(name: name)
+    }
+}
+
+let sameLabel = try decoder.decode(Label.self, from: labelData)
