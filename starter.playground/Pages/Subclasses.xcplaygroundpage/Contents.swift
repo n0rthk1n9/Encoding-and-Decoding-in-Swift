a399d1568bd9ffda04a70a24464b59f84bb667a2
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-13 15:53:52 +0200

    learn subclass encoding and decoding

--- starter.playground/Pages/Subclasses.xcplaygroundpage/Contents.swift
+++ starter.playground/Pages/Subclasses.xcplaygroundpage/Contents.swift
@@ -29,33 +29,57 @@
 import Foundation
 
 struct Toy: Codable {
-  var name: String
+    var name: String
 }
 
-class BasicEmployee {
-  var name: String
-  var id: Int
-  
-  init(name: String, id: Int) {
-    self.name = name
-    self.id = id
-  }
+class BasicEmployee: Codable {
+    var name: String
+    var id: Int
+
+    init(name: String, id: Int) {
+        self.name = name
+        self.id = id
+    }
 }
 
 class GiftEmployee: BasicEmployee {
-  var birthday: Date
-  var toy: Toy
-  
-  init(name: String, id: Int, birthday: Date, toy: Toy) {
-    self.birthday = birthday
-    self.toy = toy
-    super.init(name: name, id: id)
-  }
+    var birthday: Date
+    var toy: Toy
 
+    init(name: String, id: Int, birthday: Date, toy: Toy) {
+        self.birthday = birthday
+        self.toy = toy
+        super.init(name: name, id: id)
+    }
+
+    enum CodingKeys: CodingKey {
+        case employee, birthday, toy
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        birthday = try container.decode(Date.self, forKey: .birthday)
+        toy = try container.decode(Toy.self, forKey: .toy)
+        // 3
+        let baseDecoder = try container.superDecoder(forKey: .employee)
+        try super.init(from: baseDecoder)
+    }
+
+    override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(birthday, forKey: .birthday)
+        try container.encode(toy, forKey: .toy)
+        let baseEncoder = container.superEncoder(forKey: .employee)
+        try super.encode(to: baseEncoder)
+    }
 }
 
 let toy = Toy(name: "Teddy Bear")
 let encoder = JSONEncoder()
 let decoder = JSONDecoder()
 
-
+let giftEmployee = GiftEmployee(name: "John Appleseed", id: 7, birthday: Date(),
+                                toy: toy)
+let giftData = try encoder.encode(giftEmployee)
+let giftString = String(data: giftData, encoding: .utf8)!
+let sameGiftEmployee = try decoder.decode(GiftEmployee.self, from: giftData)
