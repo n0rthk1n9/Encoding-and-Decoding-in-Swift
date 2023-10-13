0156e99533674c58642bb20cb44d0b8a6a446040
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-13 15:49:42 +0200

    learn date encoding and decoding

--- starter.playground/Pages/Dates.xcplaygroundpage/Contents.swift
+++ starter.playground/Pages/Dates.xcplaygroundpage/Contents.swift
@@ -28,21 +28,33 @@
 import Foundation
 
 struct Toy: Codable {
-  var name: String
+    var name: String
 }
 
 let encoder = JSONEncoder()
 let decoder = JSONDecoder()
 
 struct Employee: Codable {
-  var name: String
-  var id: Int
-  var birthday: Date
-  var toy: Toy
+    var name: String
+    var id: Int
+    var birthday: Date
+    var toy: Toy
 }
 
 let toy = Toy(name: "Teddy Bear")
 let employee = Employee(name: "John Appleseed", id: 7, birthday: Date(), toy: toy)
+
+extension DateFormatter {
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        return formatter
+    }()
+}
+
+// 2
+encoder.dateEncodingStrategy = .formatted(.dateFormatter)
+decoder.dateDecodingStrategy = .formatted(.dateFormatter)
 
 let dateData = try encoder.encode(employee)
 let dateString = String(data: dateData, encoding: .utf8)!
